622b21f5eba222367d06dbdeb871d7ede72c52bf
Moy Olvera <moi6olvera@gmail.com>
2022-03-13 17:51:47 -0600

    Added firebase config

--- App.tsx
+++ App.tsx
@@ -1,4 +1,9 @@
+import { initializeApp } from 'firebase/app';
+import { FIREBASE_CONFIG } from '@utils';
+
 import AppNavigator from './src/AppNavigator';
+
+initializeApp(FIREBASE_CONFIG);
 
 function App() {
   return <AppNavigator />;
---
+++ src/utils/firebase.ts
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyBplcPGlQVhjomC_dSxRzit702UtPGTUf4',
+  authDomain: 'montajtli.firebaseapp.com',
+  projectId: 'montajtli',
+  storageBucket: 'montajtli.appspot.com',
+  messagingSenderId: '79677150314',
+  appId: '1:79677150314:web:61c61eb38903af27c2e613',
+  measurementId: 'G-SNXZFGHXZV'
+};
+
+export default FIREBASE_CONFIG;
